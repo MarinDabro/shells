7401026c42c11fd067c97a66e8d61050269049c6
marin <mdabro85@gmail.com>
2022-07-21 21:59:24 +0200

    :)

--- opacityAlacritty.sh
+++ opacityAlacritty.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-if grep "opacity: 0.7" ~/.config/alacritty/alacritty.yml
+if grep "opacity: 0.8" ~/.config/alacritty/alacritty.yml
 then
-    sed -i 's/opacity: 0.7/opacity: 0.95/' ~/.config/alacritty/alacritty.yml
+    sed -i 's/opacity: 0.8/opacity: 1/' ~/.config/alacritty/alacritty.yml
 else #if sed -i 's/opacity: 1/opacity: 1/' ~/.config/alacritty/alacritty.yml
 #then
-    sed -i 's/opacity: 0.95/opacity: 0.7/' ~/.config/alacritty/alacritty.yml
+    sed -i 's/opacity: 1/opacity: 0.8/' ~/.config/alacritty/alacritty.yml
 fi
