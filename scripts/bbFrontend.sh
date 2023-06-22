aeeda244697f4b155140a7851497b25c11e52475
marin <mdabro85@gmail.com>
2023-06-22 13:33:10 +0200

    new aliases

--- scripts/bbFrontend.sh
+++ scripts/bbFrontend.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-alacritty --working-directory ~/Projects/companylinks/product/company/dev/app_frontend &
-alacritty --working-directory ~/Projects/companylinks/product/platform/dev/app_frontend &
-alacritty --working-directory ~/Projects/companylinks/product/investor/dev/app_frontend &
-alacritty --working-directory ~/Projects/companylinks/product/advisor/dev/app_frontend &
+alacritty --working-directory ~/bboerse/Projects/product/company/dev/app_frontend &
+alacritty --working-directory ~/bboerse/Projects/product/platform/dev/app_frontend &
+alacritty --working-directory ~/bboerse/Projects/product/investor/dev/app_frontend &
+alacritty --working-directory ~/bboerse/Projects/product/advisor/dev/app_frontend &
 
+code ~/bboerse/Projects/product/company/dev/app_frontend &
+code ~/bboerse/Projects/product/platform/dev/app_frontend &
+code ~/bboerse/Projects/product/investor/dev/app_frontend &
+code ~/bboerse/Projects/product/advisor/dev/app_frontend &
